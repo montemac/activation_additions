734571d7ea1eee07f43a02af258458750f90f401
montemac <m.macdiarmid@gmail.com>
2023-03-17 15:04:40 -0700

    Change to pandas version for colab compat

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
         'transformer-lens @ git+https://github.com/neelnanda-io/TransformerLens.git@1f65f4bf35d91677deea2331561ae6a9d2e92d38',
         'torch>=1.13.1',
         'numpy>=1.22.1',
-        'pandas>=1.5.3',
+        'pandas>=1.4.4',
         'jaxtyping>=0.2.14',
         'prettytable>=3.6.0',
         'ipywidgets>=7.7',
