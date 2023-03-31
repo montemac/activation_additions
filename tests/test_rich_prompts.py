6759c912537a83c6bce5f946fef19856a062a2e0
Alex Turner <turneral1@grinnell.edu>
2023-03-31 15:25:44 -0700

    Added basic tests; superposition tests currently broken

---
+++ tests/test_rich_prompts.py
@@ -0,0 +1,40 @@
+""" Tests for the rich_prompts module. """
+
+from algebraic_value_editing.rich_prompts import RichPrompt
+from algebraic_value_editing import rich_prompts
+
+
+def test_creation():
+    """Test that we can create a RichPrompt."""
+    rich_prompt = RichPrompt(
+        prompt="Hello world!", act_name="encoder", coeff=1.0
+    )
+    assert rich_prompt.prompt == "Hello world!"
+    assert rich_prompt.act_name == "encoder"
+    assert rich_prompt.coeff == 1.0
+
+
+def test_x_vector_creation():
+    """Test that we can create a RichPrompt's x_vector."""
+    rich_prompt_positive = RichPrompt(
+        prompt="Hello world!", act_name="", coeff=1.0
+    )
+    rich_prompt_negative = RichPrompt(
+        prompt="Goodbye world!", act_name="", coeff=-1.0
+    )
+
+    x_vector_positive, x_vector_negative = rich_prompts.get_x_vector(
+        prompt1="Hello world!",
+        prompt2="Goodbye world!",
+        coeff=1.0,
+        act_name="",
+    )
+
+    # Check that the x_vectors are the same as the RichPrompts
+    for xvec, rch_prompt in zip(
+        [x_vector_positive, x_vector_negative],
+        [rich_prompt_positive, rich_prompt_negative],
+    ):
+        assert xvec.prompt == rch_prompt.prompt
+        assert xvec.act_name == rch_prompt.act_name
+        assert xvec.coeff == rch_prompt.coeff
---
+++ tests/test_superposition.py
@@ -0,0 +1,74 @@
+""" Tests for superimposing prompts. """
+from typing import Dict
+
+import torch
+from transformer_lens.HookedTransformer import HookedTransformer
+from jaxtyping import Float
+from algebraic_value_editing import rich_prompts
+
+# Load GPT-2 small using transformerlens
+model: HookedTransformer = HookedTransformer.from_pretrained(
+    model_name="gpt2-small"
+)
+
+
+def test_superposition_hello_bye():
+    """Test that we can get the superposition of two prompts."""
+    weighted_prompts: Dict[str, float] = {
+        "Hello": 1.0,
+        "Bye": 1.0,
+    }
+    superposition: Float[
+        torch.Tensor, "batch pos"
+    ] = rich_prompts.weighted_prompt_superposition(
+        model=model, weighted_prompts=weighted_prompts
+    )
+    assert superposition.shape == (1, 3)
+
+
+def test_superposition_empty():
+    """Test handling of trivial superposition."""
+    weighted_prompts: Dict[str, float] = {
+        "": 1.0,
+    }
+    superposition: Float[
+        torch.Tensor, "batch pos"
+    ] = rich_prompts.weighted_prompt_superposition(
+        model=model, weighted_prompts=weighted_prompts
+    )
+    assert superposition.shape == (1, 1)
+
+
+def test_superposition_cancel():
+    """Test that superimposing two prompts with opposite coefficients
+    and a shared first token will cancel at index 1."""
+    weighted_prompts: Dict[str, float] = {
+        "A B": 1.0,
+        "A C": -1.0,
+    }
+    superposition: Float[
+        torch.Tensor, "batch pos"
+    ] = rich_prompts.weighted_prompt_superposition(
+        model=model, weighted_prompts=weighted_prompts
+    )
+    assert superposition.shape == (1, 3)
+    assert (
+        superposition[0, 0] == 50256
+    )  # EOF token is preserved in superposition
+    assert superposition[0, 1] == 0  # Cancellation occurs at index 1
+
+
+def test_superposition_cancel_all():
+    """Test that superimposing two prompts with opposite coefficients
+    will yield the zero tensor, given that the initial token is not fixed."""
+    weighted_prompts: Dict[str, float] = {
+        "A B": 1.0,
+        "A C": -1.0,
+    }
+    superposition: Float[
+        torch.Tensor, "batch pos"
+    ] = rich_prompts.weighted_prompt_superposition(
+        model=model, weighted_prompts=weighted_prompts, fix_init_tok=False
+    )
+    assert superposition.shape == (1, 3)
+    assert superposition[0, 0] == 0  # Cancellation occurs at index 0, since
