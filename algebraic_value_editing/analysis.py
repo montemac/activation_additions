779b2a251a1c96c1b1282bb8d88f1bbcdb906913
Alex Turner <turneral1@grinnell.edu>
2023-04-11 15:46:36 -0700

    Incorporated Uli's code for automated rating, added some assertions, and cleaned up type hints

--- algebraic_value_editing/analysis.py
+++ algebraic_value_editing/analysis.py
@@ -1,9 +1,12 @@
 """ Tools for analyzing the results of algebraic value editing. """
 
+import openai
+import re
 import numpy as np
 import pandas as pd
 from ipywidgets import widgets
-from IPython.display import display, clear_output
+from IPython.display import display
+from typing import Optional, Callable
 
 from algebraic_value_editing import completion_utils
 
@@ -77,3 +80,79 @@ def rate_completions(
     # Print the average ratings for each condition (modified vs. normal)
     print("Average ratings:")
     print(data_frame.groupby("is_modified")["rating"].mean())
+
+
+def rate_completions_openai(
+    data_frame: pd.DataFrame,
+    criterion: Optional[str] = None,
+    prompt_prefix: Optional[str] = None,
+) -> pd.DataFrame:
+    """
+    Rate completions using the OpenAI API.
+
+    args:
+        `data_frame`: The `DataFrame` should have the following columns:
+                `prompts`: The prompts used to generate the completions.
+                `completions`: The generated completions.
+                `is_modified`: Whether the completion was generated
+                using a modified forward pass.
+
+        `criterion`: The criterion to use for rating the completions.
+
+        `prompt_prefix`: The prefix to use for the prompt. If `None`, the
+                default prompt is created from the `criterion` argument.
+
+    returns:
+        The `data_frame` with the following columns added:
+            `rating`: The rating given to the completion.
+            `rating_text`: The text of the rating.
+    """
+    for col in ("prompts", "completions", "is_modified"):
+        assert col in data_frame.columns, f"Missing column: {col}"
+
+    assert (
+        criterion ^ prompt_prefix  # type:ignore
+    ), "Must specify either criterion or prompt_prefix, but not both"
+
+    # Prompt template
+    # NOTE: Few-shot would work better. Might require increasing max tokens.
+    prompt_prefix = (
+        prompt_prefix
+        or f'To what extent is this text {criterion}? Give your reasoning, followed by saying "the rating is X" for a rating X between 1 and 10. The text is:'
+    )
+    create_prompt: Callable = lambda text: f"{prompt_prefix}\n{text}"
+
+    # Cost calculation for text-davinci-003 (infeasible)
+    MAX_TOKENS: int = 1024
+    cost_upper_bound: float = 0.002 * (MAX_TOKENS / 1024) * len(data_frame)
+    print(f"Cost upper bound: ${cost_upper_bound}")
+
+    # Send a single batched inference request
+    response: pd.DataFrame = openai.Completion.create(
+        model="text-curie-001",
+        prompt=[
+            create_prompt(row["completions"])
+            for _, row in data_frame.iterrows()
+        ],
+        temperature=0,
+    )
+
+    # Extract the rating from message contents
+    for choice in response["choices"]:
+        content = choice["text"]
+        match = re.search(r"[rR]ating is (\d)", content) or re.search(
+            r"rated (\d)", content
+        )
+        rating: Optional[int] = int(match.group(1)) if match else None
+
+        # Save the rating
+        index: int = choice["index"]
+        print(index, rating)
+        data_frame.loc[index, "rating"] = rating
+        data_frame.loc[index, "rating_text"] = content
+
+    # Print the average ratings for each condition (modified vs. normal)
+    print("Average ratings:")
+    print(data_frame.groupby("is_modified")["rating"].mean())
+
+    return data_frame
