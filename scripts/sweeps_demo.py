bf76503ee712817bd73fd585ce98d16f270cf872
montemac <m.macdiarmid@gmail.com>
2023-04-17 11:00:33 -0700

    Minor tweak to sweeps demo notebook

--- scripts/sweeps_demo.py
+++ scripts/sweeps_demo.py
@@ -132,6 +132,7 @@ except FileNotFoundError:
 
 # %%
 # Visualize
+# Reduce the patched DataFrame
 reduced_df = patched_df.groupby(["prompts", "rich_prompt_index"]).mean(
     numeric_only=True
 )
@@ -139,28 +140,76 @@ reduced_joined_df = reduced_df.join(
     rich_prompts_df, on="rich_prompt_index"
 ).reset_index()
 
+# Reduce the normal DataFrame
+reduced_normal_df = normal_df.groupby(["prompts"]).mean(numeric_only=True)
 
-def plot_col(data, col_to_plot, title):
+
+# Plot function
+def plot_col(
+    data,
+    col_to_plot,
+    title,
+    col_x="coeff",
+    col_color="act_name",
+    baseline_data=None,
+):
     """Plot a column, with colors/facets/x set."""
-    px.line(
+    fig = px.line(
         data,
         title=title,
-        x="coeff",
+        color=col_color,
         y=col_to_plot,
-        color="act_name",
+        x=col_x,
         facet_col="prompts",
-    ).show()
+    )
+    if baseline_data is not None and col_to_plot in baseline_data:
+        for ii, prompt in enumerate(baseline_data.index):
+            fig.add_hline(
+                y=baseline_data.loc[prompt][col_to_plot],
+                row=1,
+                col=ii + 1,
+                annotation_text="normal",
+                annotation_position="bottom left",
+            )
+    fig.show()
 
 
-plot_col(
-    reduced_joined_df, "wedding_words_count", "Average wedding word count"
-)
-plot_col(reduced_joined_df, "loss", "Average loss")
+# plot_col(
+#     reduced_joined_df, "wedding_words_count", "Average wedding word count"
+# )
+# plot_col(reduced_joined_df, "loss", "Average loss")
 
+# Exlude the extreme coeffs, likely not that interesting
 reduced_joined_filt_df = reduced_joined_df[
     (reduced_joined_df["coeff"] >= -4) & (reduced_joined_df["coeff"] <= 4)
 ]
+
+# Plot
 plot_col(
-    reduced_joined_filt_df, "wedding_words_count", "Average wedding word count"
+    reduced_joined_filt_df,
+    "wedding_words_count",
+    "Average wedding word count",
+    baseline_data=reduced_normal_df,
 )
-plot_col(reduced_joined_filt_df, "loss", "Average loss")
+plot_col(
+    reduced_joined_filt_df,
+    "loss",
+    "Average loss",
+    baseline_data=reduced_normal_df,
+)
+plot_col(
+    reduced_joined_filt_df,
+    "wedding_words_count",
+    "Average wedding word count",
+    col_x="act_name",
+    col_color="coeff",
+    baseline_data=reduced_normal_df,
+)
+plot_col(
+    reduced_joined_filt_df,
+    "loss",
+    "Average loss",
+    col_x="act_name",
+    col_color="coeff",
+    baseline_data=reduced_normal_df,
+)
