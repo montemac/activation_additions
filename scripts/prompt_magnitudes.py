Fixed issue where residual stream fraction computation was taking mean over loss as well. Added notebook which Peli used to investigate sampling kwargs

scripts/prompt_magnitudes.py
@@ -30,7 +30,6 @@ except ImportError:
         ),
     )
 
-
 # %%
 import torch
 import pandas as pd
@@ -41,16 +40,16 @@ from transformer_lens.HookedTransformer import HookedTransformer
 from algebraic_value_editing import hook_utils, prompt_utils
 from algebraic_value_editing.prompt_utils import RichPrompt
 
-
 # %%
 device: str = "cpu"
 model_name = "gpt2-xl"
-model: HookedTransformer = HookedTransformer.from_pretrained(model_name, device="cpu")
+model: HookedTransformer = HookedTransformer.from_pretrained(
+    model_name, device="cpu"
+)
 _ = model.to(device)
 
 _ = torch.set_grad_enabled(False)
 torch.manual_seed(0)  # For reproducibility
-
 
 # %% [markdown]
 # Let's examine what the residual stream magnitudes tend to be, by taking the Frobenius
@@ -71,9 +70,9 @@ if response.status_code == 200:
     print(f"Downloaded {len(prompts)} prompts")
 else:
     raise Exception(
-        f"Failed to download the file: {response.status_code} -" f" {response.reason}"
+        f"Failed to download the file: {response.status_code} -"
+        f" {response.reason}"
     )
-
 
 # %%
 DF_COLS: List[str] = [
@@ -90,7 +89,6 @@ sampling_kwargs: Dict[str, float] = {
 }
 
 num_layers: int = model.cfg.n_layers
-
 
 # %% [markdown]
 # ## Residual stream magnitudes increase exponentially with layer number
@@ -125,9 +123,7 @@ import numpy as np
 def magnitude_histogram(df: pd.DataFrame) -> go.Figure:
     """Plot a histogram of the residual stream magnitudes for each layer
     of the network."""
-    assert (
-        "Magnitude" in df.columns
-    ), "Dataframe must have a 'Magnitude' column"
+    assert "Magnitude" in df.columns, "Dataframe must have a 'Magnitude' column"
 
     df["LogMagnitude"] = np.log10(df["Magnitude"])
 
@@ -135,9 +131,7 @@ def magnitude_histogram(df: pd.DataFrame) -> go.Figure:
     num_unique_activation_locations = df["Activation Location"].nunique()
 
     # Generate a color list that is long enough to accommodate all unique activation locations
-    extended_rainbow = (
-        px.colors.sequential.Rainbow * num_unique_activation_locations
-    )
+    extended_rainbow = px.colors.sequential.Rainbow * num_unique_activation_locations
     color_list = extended_rainbow[:num_unique_activation_locations]
 
     fig = px.histogram(
@@ -160,6 +154,7 @@ def magnitude_histogram(df: pd.DataFrame) -> go.Figure:
     )
 
     return fig
+
 
 
 # %%
@@ -192,11 +187,9 @@ for act_loc in activation_locations_8:
         # Append the new row to the dataframe
         prompt_df = pd.concat([prompt_df, row], ignore_index=True)
 
-
 # %%
 fig: go.Figure = magnitude_histogram(prompt_df)
 fig.show()
-
 
 # %% [markdown]
 # In GPT2-XL, the fast magnitude gain
@@ -230,7 +223,6 @@ for act_loc in activation_locations:
 
 fig: go.Figure = magnitude_histogram(first_7_df)
 fig.show()
-
 
 
 # %% [markdown]
@@ -267,7 +259,9 @@ for layer in range(model.cfg.n_layers):
     )
     random_embed = torch.randn(1000, model.cfg.d_model).to(device)
     random_embed /= random_embed.norm(dim=-1, keepdim=True)
-    OV_output = torch.zeros(1000, model.cfg.n_heads + 1, model.cfg.d_model).to(device)
+    OV_output = torch.zeros(1000, model.cfg.n_heads + 1, model.cfg.d_model).to(
+        device
+    )
     OV_output[:, : model.cfg.n_heads, :] = einsum(
         "batch embed, head embed embedout -> batch head embedout",
         random_embed,
@@ -325,11 +319,11 @@ fig = px.scatter(
     color="Head",
     log_y=True,
     title=(
-        "How much the W_OV matrices increase the norm of the input by layer" " and head"
+        "How much the W_OV matrices increase the norm of the input by layer"
+        " and head"
     ),
 )
 fig.show()
-
 
 
 # %% [markdown]
@@ -371,7 +365,6 @@ plt.scatter(
 plt.xlabel("layer")
 plt.ylabel("Std of random OV-output")
 plt.title(model_name)
-
 
 
 # %% [markdown]
@@ -419,7 +412,6 @@ fig = px.scatter(
     title="Frobenius norms",
 )
 fig.show()
-
 
 
 # %% [markdown]
@@ -485,7 +477,6 @@ fig = px.scatter(
     title="How much the MLP increase the norm of the input, by layer",
 )
 fig.show()
-
 
 
 # %% [markdown]
@@ -598,7 +589,10 @@ for layer in range(model.cfg.n_layers):
                         layer,
                         (
                             ReLU_zero_rate
-                            * (Winout_mean_norm_increase + bin_mean_norm_increase)
+                            * (
+                                Winout_mean_norm_increase
+                                + bin_mean_norm_increase
+                            )
                             + bout_mean_norm_increase
                         ).item(),
                         "Naive total",
@@ -620,7 +614,6 @@ fig = px.line(
     title="MLP components",
 )
 fig.show()
-
 
 
 # %% [markdown]
@@ -656,12 +649,12 @@ def line_plot(
     fig.update_layout(
         legend_title_text=legend_title_text,
         title=title,
+        width=600,
         xaxis_title="Layer Number",
         yaxis_title=f"Norm{' (log 10)' if log_y else ''}",
     )
 
     return fig
-
 
 
 # %%
@@ -693,7 +686,6 @@ for act_loc in all_resid_pre_locations:
             # Append the new row to the dataframe
             addition_df = pd.concat([addition_df, row], ignore_index=True)
 
-
 # %%
 for use_log in (True, False):
     fig = line_plot(
@@ -701,8 +693,8 @@ for use_log in (True, False):
         log_y=use_log,
         title=f"Residual Stream Norm by Layer Number in {model_name}",
     )
-    fig.update_layout(width=600)
     fig.show()
+
 
 # %% [markdown]
 # To confirm the exponential increase in magnitude, let's plot the
@@ -748,7 +740,6 @@ for act_loc in all_resid_pre_locations:
 
     mags_prev = mags
 
-
 # %%
 relative_fig = line_plot(
     relative_df,
@@ -765,9 +756,9 @@ relative_fig.update_yaxes(range=[0.9, 1.5])
 
 # Plot a horizontal line at y=1
 relative_fig.add_hline(y=1, line_dash="dash", line_color="black")
-relative_fig.update_layout(width=600)
 
 relative_fig.show()
+
 
 # %%
 # Print the geometric mean of the magnitude growth rates
@@ -778,7 +769,6 @@ for pos in range(6):
         f"The `{tokens[pos]}` token (position {pos}) has an average growth"
         f" rate of {geom_avg:.3f}"
     )
-
 
 # %% [markdown]
 # The exponential increase in magnitude is confirmed, with tokens having

tests/test_hook_utils.py
@@ -18,6 +18,7 @@ def fixture_model() -> HookedTransformer:
         model_name="attn-only-2l", device="cpu"
     )
 
+
 def test_hook_fn_from_slice():
     """Test that we can selectively modify a portion of the residual stream."""
     input_tensor: torch.Tensor = torch.zeros((1, 2, 4))
@@ -35,15 +36,18 @@ def test_hook_fn_from_slice():
 
     assert torch.eq(result_tens, target_tens).all(), "Slice test failed"
 
+
 def test_hook_fn_from_activations():
-    """Testing the front and back modifiers of the xvec_position"""
+    """Testing the front and back modifiers of the addition_location"""
     input_tensor: torch.Tensor = torch.ones((1, 10, 1))
     activations: torch.Tensor = 2 * torch.ones((1, 4, 1))
 
     back_target: torch.Tensor = torch.tensor([[1, 1, 1, 1, 1, 1, 3, 3, 3, 3]])
     back_target: torch.Tensor = back_target.unsqueeze(0).unsqueeze(-1)
 
-    hook_fxn: Callable = hook_utils.hook_fn_from_activations(activations=activations, addition_location="back")
+    hook_fxn: Callable = hook_utils.hook_fn_from_activations(
+        activations=activations, addition_location="back"
+    )
     result: torch.Tensor = hook_fxn(input_tensor)
 
     assert torch.eq(result, back_target).all(), "xvec = back test failed"
@@ -55,7 +59,9 @@ def test_hook_fn_from_activations():
     front_target: torch.Tensor = torch.tensor([[3, 3, 3, 3, 1, 1, 1, 1, 1, 1]])
     front_target: torch.Tensor = front_target.unsqueeze(0).unsqueeze(-1)
 
-    hook_fxn: Callable = hook_utils.hook_fn_from_activations(activations=activations, addition_location="front")
+    hook_fxn: Callable = hook_utils.hook_fn_from_activations(
+        activations=activations, addition_location="front"
+    )
     result: torch.Tensor = hook_fxn(input_tensor)
 
     assert torch.eq(result, front_target).all(), "xvec = front test failed"