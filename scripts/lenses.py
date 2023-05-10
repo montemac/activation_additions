fe75b7cfc4c330510894284a3d0fe87cad1e9a1d
Ulisse Mini <ulisse.mini@gmail.com>
2023-05-10 15:21:27 -0700

    Mad science with big coeffs and tuned lens

--- scripts/lenses.py
+++ scripts/lenses.py
@@ -8,6 +8,7 @@ from transformer_lens.HookedTransformer import HookedTransformer
 
 from tuned_lens import TunedLens
 from tuned_lens.plotting import PredictionTrajectory
+from tqdm import tqdm
 import numpy as np
 import torch
 
@@ -64,10 +65,11 @@ def apply_metric(metric: Metric, pt: PredictionTrajectory):
 
 
 def plot_lens_diff(
-    caches: List[Dict[str, torch.Tensor]],
-    dataframes: List[pd.DataFrame],
+    traj_normal: PredictionTrajectory,
+    traj_modified: PredictionTrajectory,
     metric: Metric,
     layer_stride: int = 4,
+    title_text: str = "Tokens visualized with the Tuned Lens",
 ):
     fig = make_subplots(
         rows=2,
@@ -77,13 +79,11 @@ def plot_lens_diff(
         # subplot_titles=("Entropy", "Forward KL", "Cross Entropy", "Max Probability"),
     )
 
-    fig.update_layout(height=1000, width=800, title_text="Tokens visualized with the Tuned Lens")
-
-    trajectories = prediction_trajectories(caches, dataframes, model.tokenizer, tuned_lens)
+    fig.update_layout(height=1000, width=800, title=title_text)
 
     # Update heatmap data inside playground function
-    hm_normal = apply_metric(metric, trajectories[0]).heatmap(layer_stride=layer_stride)
-    hm_modified = apply_metric(metric, trajectories[1]).heatmap(layer_stride=layer_stride)
+    hm_normal = apply_metric(metric, traj_normal).heatmap(layer_stride=layer_stride)
+    hm_modified = apply_metric(metric, traj_modified).heatmap(layer_stride=layer_stride)
 
     fig.add_trace(hm_normal, row=1, col=1)
     fig.add_trace(hm_modified, row=2, col=1)
@@ -92,34 +92,137 @@ def plot_lens_diff(
 
 # Main playground for lenses. Run with ctrl+enter
 
-prompt = "I hate you because"
+coeffs = [2**i for i in range(20)]
 
-rich_prompts = [
-    *get_x_vector(
-        prompt1="Love",
-        prompt2="Hate",
-        coeff=5,
-        act_name=6,
-        pad_method="tokens_right",
-        model=model,
-        custom_pad_id=model.to_single_token(" "),
+traj_list_by_coeff: Dict[str, PredictionTrajectory] = {}
+
+for coeff in tqdm(coeffs):
+    prompt = "I hate you because"
+
+    rich_prompts = [
+        *get_x_vector(
+            prompt1="Love",
+            prompt2="Hate",
+            coeff=coeff,
+            act_name=6,
+            pad_method="tokens_right",
+            model=model,
+            custom_pad_id=model.to_single_token(" "),
+        )
+    ]
+
+    dataframes, caches = run_hooked_and_normal_with_cache(
+        model=model, rich_prompts=rich_prompts,
+        kw=dict(prompt_batch=[prompt] * 1, tokens_to_generate=0, top_p=0.3, seed=0),
     )
-]
 
-dataframes, caches = run_hooked_and_normal_with_cache(
-    model=model, rich_prompts=rich_prompts,
-    kw=dict(prompt_batch=[prompt] * 1, tokens_to_generate=6, top_p=0.3, seed=0),
-)
+    traj_normal, traj_modified = prediction_trajectories(caches, dataframes, model.tokenizer, tuned_lens)
+    traj_list_by_coeff[coeff] = traj_modified
 
-trajectories = prediction_trajectories(caches, dataframes, model.tokenizer, tuned_lens)
+    # fig = plot_lens_diff(
+    #     traj_normal=traj_normal,
+    #     traj_modified=traj_modified,
+    #     metric='entropy',
+    #     layer_stride=2,
+    #     title_text=f"Tokens visualized with the Tuned Lens, coeff={coeff}",
+    # )
+    # fig.show()
 
-fig = plot_lens_diff(
-    caches=caches,
-    dataframes=dataframes,
-    metric='entropy',
-    layer_stride=2,
+
+# %%
+# Animate traj modified heatmap for every coeff. use frames
+
+import plotly.graph_objects as go
+
+layer_stride = 4
+hm_templ = traj_list_by_coeff[coeffs[0]].entropy().heatmap(layer_stride=layer_stride)
+xmax, ymax = hm_templ.customdata.shape[:-1]
+
+fig = go.Figure(
+    data=[hm_templ],
+    layout=go.Layout(
+        # get xaxis from template heatmap
+        xaxis=dict(autorange=True),
+        yaxis=dict(autorange=True),
+        title_text="Tokens visualized with the Tuned Lens",
+        updatemenus=[dict(
+            type="buttons",
+            buttons=[
+                dict(label="Play", method="animate", args=[
+                    None
+                    # dict(frame=dict(duration=500, redraw=False), fromcurrent=True, mode="immediate"),
+                ])
+            ],
+            direction="left",
+            xanchor="right", yanchor="top", # required for right/top padding
+            pad={"r": 10, "t": 100},
+        )],
+    ),
+    frames=[
+        go.Frame(
+            data=[
+                apply_metric('entropy', traj).heatmap(layer_stride=layer_stride)
+            ],
+            layout=go.Layout(
+                title_text=f"Tokens visualized with the Tuned Lens, coeff={coeff}",
+            ),
+        )
+        for coeff, traj in traj_list_by_coeff.items()
+    ]
 )
 fig.show()
+
+# %%
+
+hm_templ.customdata.shape
+
+# %%
+# Plot kl divergence (coeff, next coeff) for each layer
+
+def js_div(lp1, lp2):
+    js_div = 0.5 * np.sum(
+        lp1 * (lp1 - lp2), axis=-1
+    ) + 0.5 * np.sum(lp2 * (lp2 - lp1), axis=-1)
+    return np.log(1+js_div)
+
+ys = np.array([
+    js_div(
+        traj_list_by_coeff[coeffs[i]].log_probs,
+        traj_list_by_coeff[coeffs[i+1]].log_probs,
+    )
+    for i in range(len(coeffs)-1)
+])
+
+
+# Plot it
+
+import plotly.graph_objects as go
+
+fig = go.Figure()
+
+for i in [48]: # range(ys.shape[1]):
+    for j in range(ys.shape[2]):
+        fig.add_trace(
+            go.Scatter(
+                x=coeffs[:-1],
+                y=ys[:, i, j],
+                mode="lines+markers",
+                name=f"layer {i}, token {j}",
+            )
+        )
+
+fig.update_layout(
+    title="JS Divergence between adjacent coefficients",
+    xaxis_title="Coefficient",
+    yaxis_title="Log(1 + JS-Divergence)",
+    legend_title="Layer, Token",
+    # log both scales
+    xaxis_type="log",
+    # yaxis_type="log",
+)
+fig.show()
+
+
 
 # %%
 # Play with printing completions to check behavior
