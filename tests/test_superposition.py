2ba2fd5612f0e16ebfa53883357ea04f716d5164
Alex Turner <turneral1@grinnell.edu>
2023-05-14 20:09:45 -0700

    Fixed superposition unit tests

---
+++ tests/test_superposition.py
@@ -0,0 +1,44 @@
+""" Tests for superimposing prompts. """
+from typing import Dict, List
+import pytest
+
+from transformer_lens.HookedTransformer import HookedTransformer
+from algebraic_value_editing import prompt_utils
+from algebraic_value_editing.prompt_utils import ActivationAddition
+
+
+@pytest.fixture(name="model", scope="module")
+def fixture_model() -> HookedTransformer:
+    """Test fixture that returns a small pre-trained transformer."""
+    return HookedTransformer.from_pretrained(
+        model_name="attn-only-1l", device="cpu"
+    )
+
+
+def test_superposition_hello_bye(model):
+    """Test that we can get the superposition of two prompts."""
+    weighted_prompts: Dict[str, float] = {
+        "Hello": 1.0,
+        "Bye": 1.0,
+    }
+    superpos_act_adds: List[ActivationAddition] = (
+        prompt_utils.weighted_prompt_superposition(
+            model=model, weighted_prompts=weighted_prompts
+        )
+    )
+    dummy_add = superpos_act_adds[-1]
+    assert dummy_add.tokens.shape[0] == 2  # only 2 tokens needed for dummy add
+
+
+def test_superposition_empty(model):
+    """Test handling of trivial superposition."""
+    weighted_prompts: Dict[str, float] = {
+        "": 1.0,
+    }
+    superpos_act_adds: List[ActivationAddition] = (
+        prompt_utils.weighted_prompt_superposition(
+            model=model, weighted_prompts=weighted_prompts
+        )
+    )
+    dummy_add = superpos_act_adds[-1]
+    assert dummy_add.tokens.shape[0] == 1
