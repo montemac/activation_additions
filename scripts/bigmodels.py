fbab28d0053b48fc2f0c022c82f2983cd6595e2d
DavidUdell <udelldavidb@gmail.com>
2023-07-15 00:32:28 +0000

    Fiddling with existing bigmodels code.

--- scripts/bigmodels.py
+++ scripts/bigmodels.py
@@ -1,55 +1,58 @@
-# %% [markdown]
-# # Big Models
-# Running the algebraic value editing experiments on large models.
-# ## Downloading the models
-# (TODO: Grab docs from laptop)
-
-
 # %%
-
-from transformer_lens import HookedTransformer
-from transformers import LlamaForCausalLM, LlamaTokenizer, AutoModelForCausalLM, AutoTokenizer, GPT2LMHeadModel, GenerationConfig
+"""Script to run activation engineering on Vicuna 13B."""
 from contextlib import contextmanager
-from typing import Tuple, List, Callable, Optional
+from typing import Tuple, Callable, Optional
 import torch as t
 import torch.nn as nn
 import numpy as np
+from transformers import LlamaForCausalLM, LlamaTokenizer, GenerationConfig
 
 
 # %%
-# Load from vicuna-13B
+MODEL_DIR: str = "lmsys/vicuna-13B-v1.3"
+DEVICE: str = "cuda:0"
+DO_SAMPLE: bool = True
+TEMPERATURE: float = 1.0
+TOP_P: float = 0.9
+REPETITION_PENALTY: float = 2.0
+MAX_NEW_TOKENS: int = 50
+SEED: int = 0
+COEFF: int = 5
+ACT_NAME: int = 6
+PROMPT: str = "I hate you because"
 
-model_dir = "../vicuna-13B"
-# The operator 'aten::cumsum.out' is not currently implemented for the MPS device.I -- :(
-device = 'cpu'
+prompt_add, prompt_sub = "Love ", "Hate"
 
 t.set_grad_enabled(False)
-# tokenizer = LlamaTokenizer.from_pretrained(model_dir)
-# model = LlamaForCausalLM.from_pretrained(model_dir)
-tokenizer = AutoTokenizer.from_pretrained('gpt2-xl')
-model = AutoModelForCausalLM.from_pretrained('gpt2-xl')
-model.to(device)
-model.eval();
+tokenizer = LlamaTokenizer.from_pretrained(MODEL_DIR)
+model = LlamaForCausalLM.from_pretrained(MODEL_DIR)
+model.to(DEVICE)
+model = model.half()
+model.eval()
+
 
 # %%
+def tokenize(prompt: str) -> dict[str, t.Tensor]:
+    """Tokenize a prompt into a model input."""
+    passed_input = tokenizer(prompt, return_tensors="pt")
+    passed_input = {k: t.to(DEVICE).half() for k, t in passed_input.items()}
+    return passed_input
 
-def tokenize(prompt):
-    inputs = tokenizer(prompt, return_tensors='pt')
-    inputs = {k: t.to(device) for k, t in inputs.items()}
-    return inputs
 
-inputs = tokenize("I like cheese")
+inputs = tokenize(PROMPT)
 outputs = model(**inputs)
 
 # %%
-
-# types
+# New type declarations
 PreHookFn = Callable[[nn.Module, t.Tensor], Optional[t.Tensor]]
 Hook = Tuple[nn.Module, PreHookFn]
-Hooks = List[Hook]
+Hooks = list[Hook]
+
 
 @contextmanager
 def pre_hooks(hooks: Hooks):
+    """Context manager to register pre-forward hooks on a model."""
+    handles = []
     try:
         handles = [mod.register_forward_pre_hook(hook) for mod, hook in hooks]
         yield
@@ -58,103 +61,86 @@ def pre_hooks(hooks: Hooks):
             handle.remove()
 
 
-# Get ModuleList from arbitrary transformer model
-# (Alternatively, we could pick the module list containing >50% of model params.)
-def get_blocks(model):
-    if isinstance(model, LlamaForCausalLM):
-        return model.model.layers
-    elif isinstance(model, GPT2LMHeadModel):
-        return model.transformer.h
+def get_blocks(passed_model):
+    """Get the blocks of a model."""
+    if isinstance(passed_model, LlamaForCausalLM):
+        return passed_model.model.layers
     else:
-        raise ValueError(f"Unsupported model type: {type(model)}")
+        raise ValueError(f"Unsupported model type: {type(passed_model)}")
+
 
 @contextmanager
-def residual_stream(model: LlamaForCausalLM, layers: Optional[List[int]] = None):
-    "Context manager to track residual stream activations in the model."
-    # Plausibly could be replaced by "output_hidden_states=True" in model call. TODO: check
+def residual_stream(passed_model: LlamaForCausalLM, layers: Optional[list[int]] = None):
+    """Context manager to track residual stream activations in the model."""
+    # TODO Plausibly could be replaced by 'output_hidden_states=True' in model call.
 
-    stream = [None] * len(get_blocks(model))
+    current_stream = [None] * len(get_blocks(passed_model))
+
     def _make_hook(i):
-        def _hook(_, inputs):
-            stream[i] = inputs[0]
+        def _hook(_, current_inputs):
+            current_stream[i] = current_inputs[0]
+
         return _hook
 
-    hooks = [(layer, _make_hook(i)) for i, layer in enumerate(get_blocks(model)) if i in layers]
+    hooks = [
+        (layer, _make_hook(i))
+        for i, layer in enumerate(get_blocks(passed_model))
+        if i in layers
+    ]
     with pre_hooks(hooks):
-        yield stream
+        yield current_stream
 
 
 # %%
-# Get the residual stream
-
 with residual_stream(model, layers=[0]) as stream:
     outputs = model(**inputs)
-
 print(stream)
 
 # %%
-# AVE st
+sampling_kwargs = dict(temperature=TEMPERATURE, top_p=TOP_P)
+sampling_kwargs["repetition_penalty"] = REPETITION_PENALTY
 
-SEED = 0
-# freq_penalty isn't a thing in huggingface
-sampling_kwargs = dict(temperature=1.0, top_p=0.3) # , freq_penalty=1.0)
-sampling_kwargs['repetition_penalty'] = 2.
-
-# TODO: Automatic padding (easy)
-prompt_add, prompt_sub = 'Love ', 'Hate'
-coeff = 5
-act_name = 6
-prompt = "I hate you because"
-
+# TODO: Automatic padding
 # %%
-# Get activations. Could be made parallel, but this is fine for now.
+def get_resid_pre(prompt: str, layer_num: int):
+    """Get the residual stream activations for a prompt."""
+    with residual_stream(model, layers=[layer_num]) as working_stream:
+        model_outputs = model(**tokenize(prompt))
+    return working_stream[layer_num]
 
-def get_resid_pre(prompt: str, layer: int):
-    with residual_stream(model, layers=[layer]) as stream:
-        outputs = model(**tokenize(prompt))
-    return stream[layer]
 
-act_add = get_resid_pre(prompt_add, act_name)
-act_sub = get_resid_pre(prompt_sub, act_name)
+act_add = get_resid_pre(prompt_add, ACT_NAME)
+act_sub = get_resid_pre(prompt_sub, ACT_NAME)
 assert act_add.shape == act_sub.shape
 
 act_diff = act_add - act_sub
 
 # %%
-# Generate text from modified model
-
-# seed all the things
 t.manual_seed(SEED)
 np.random.seed(SEED)
 
 
 def _hook(_, inp):
-    resid_pre, = inp
+    (resid_pre,) = inp
     if resid_pre.shape[1] == 1:
-        return # caching in model.generate for new tokens
-    
-    # We only add to the prompt (first call), not the generated tokens.
+        return  # caching in model.generate for new tokens
+    # Only add to the first forward pass, not the later tokens.
     ppos, apos = resid_pre.shape[1], act_diff.shape[1]
     assert apos <= ppos, f"More mod tokens ({apos}) then prompt tokens ({ppos})!"
-
-    # add to the beginning (position-wise) of the activations
-    # TODO: Figure out why coeff=5 doesn't work. It works with transformerlens!
     resid_pre[:, :apos, :] += 1 * act_diff
 
 
-layer = get_blocks(model)[act_name]
+layer = get_blocks(model)[ACT_NAME]
 with pre_hooks(hooks=[(layer, _hook)]):
     outputs = model.generate(
-        **tokenize([prompt] * 5),
+        **tokenize([PROMPT] * 5),
         generation_config=GenerationConfig(
             **sampling_kwargs,
-            do_sample=True,
-            max_new_tokens=50,
+            do_sample=DO_SAMPLE,
+            max_new_tokens=MAX_NEW_TOKENS,
             eos_token_id=tokenizer.eos_token_id,
         ),
     )
 
-
 res_strs = [tokenizer.decode(o) for o in outputs]
 print(("\n\n" + "-" * 80 + "\n\n").join(res_strs))
-# %%
