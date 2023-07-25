d4d75f812eebfce21e805ad2b14e73f04d3738a5
montemac <m.macdiarmid@gmail.com>
2023-07-24 22:20:39 -0700

    In the middle of adding cmd line arguments to openwebtext experiments function, not tested yet.  Still running previous sweep in the background.

--- scripts/openwebtext_experiments.py
+++ scripts/openwebtext_experiments.py
@@ -17,6 +17,7 @@ import regex as re
 from typing import List, Optional
 import glob
 import datetime
+import argparse
 
 import numpy as np
 import pandas as pd
@@ -46,182 +47,276 @@ _ = t.set_grad_enabled(False)
 # Enable saving of plots in HTML notebook exports
 py.offline.init_notebook_mode()
 
-# Constants
-SEED = 0
-OPENWEBTEXT_FOLDER = "../../datasets/openwebtext"
-RESULTS_PARENT_FOLDER = "openwebtext_results"
+# Arguments for seed, dataset folder, results folder, etc.
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--seed",
+    type=int,
+    default=0,
+    help="Random seed to use for reproducibility",
+)
+parser.add_argument(
+    "--openwebtext_folder",
+    type=str,
+    default="../../datasets/openwebtext",
+    help="Path to the OpenWebText dataset folder",
+)
+parser.add_argument(
+    "--results_parent_folder",
+    type=str,
+    default="openwebtext_results",
+    help="Path to the parent folder for saving results",
+)
+# Optional argument for the path to a pre-generated file storing
+# relevance scores for each document in the dataset. If not provided,
+# and document text data also not provided, this will be generated.
+parser.add_argument(
+    "--relevance_data_path",
+    type=str,
+    default=None,
+    help="Path to a pre-generated file storing relevance scores "
+    "for each document in the dataset",
+)
+# Optional argument for the path to a pre-generated file storing
+# document text and relevance scores for a subset of the dataset
+# to be used for experiments.  If not provided, a new subset will be
+# generated.
+parser.add_argument(
+    "--docs_rel_by_id_path",
+    type=str,
+    default=None,
+    help="Path to a pre-generated file storing document text and "
+    "relevance scores for a subset of the dataset to be used for experiments",
+)
+# Fraction of the dataset to use for experiments, if a new subset is
+# generated, and the fraction of the subset to pick from relevant documents
+parser.add_argument(
+    "--subset_frac",
+    type=float,
+    default=0.05,
+    help="Fraction of the dataset to use for experiments",
+)
+parser.add_argument(
+    "--relevant_frac",
+    type=float,
+    default=0.5,
+    help="Fraction of the subset to pick from relevant documents",
+)
+# Length of chunks to process at a time when running forward passes
+# over the dataset
+parser.add_argument(
+    "--chunk_len",
+    type=int,
+    default=100,
+    help="Length of chunks to process at a time when running "
+    "forward passes over the dataset",
+)
+# String in format X/Y to specify the slices of the final chunks to
+# operate on in this process, allowing multiple processes to run in
+# parallel on different GPUs
+# parser.add_argument(
+#     "--chunk_slice",
+#     type=str,
+#     default="1/1",
+#     help="String in format X/Y to specify the slices of the final "
+#     "chunks to operate on in this process, allowing multiple processes "
+#     "to run in parallel on different GPUs",
+# )
+# # Device to use
+# parser.add_argument(
+#     "--device",
+#     type=str,
+#     default="cuda:0",
+#     help="Device to use",
+# )
+
+# Parse agruments
+args = parser.parse_args()
+
+# %%
+# Scan the dataset to assign a relevance score to each document
+# (density of relevant keywords)
+# NOTE: this will take ~25m to run
+RELEVANCE_KEYWORDS = [
+    "wedding",
+    "weddings",
+    "wed",
+    "marry",
+    "married",
+    "marriage",
+    "bride",
+    "groom",
+    "honeymoon",
+]
+
+
+def load_docs_and_ids(path: str):
+    """Load documents and associated ids from a file in the OpenWebText
+    dataset."""
+    ID_FIELD_LEN = 44  # Used to filter out metadata after splitting on nulls
+    # Open file and extract documents
+    with open(path, "r", encoding="utf-8") as file:
+        fields = re.split("\x00+", file.read())
+        id = None
+        ids = []
+        docs = []
+        for field in fields:
+            if field.endswith(".txt") and len(field) == ID_FIELD_LEN:
+                id = field
+            elif id is not None and len(field) > ID_FIELD_LEN:
+                ids.append(id)
+                docs.append(field)
+    return ids, docs
+
+
+def get_relevance_of_docs_in_file(
+    path: str, relevance_keywords: Optional[List[str]]
+):
+    """Load documents from a file in the OpenWebText dataset, optionally
+    calculating the relevance score for each document."""
+    ids, docs = load_docs_and_ids(path)
+    # Calculate a relevance score (density of relevant keywords) for
+    # each doc: the number of relevance keywords in the doc divided by the
+    # number of words in the doc.
+    rows = []
+    for doc in docs:
+        words = doc.lower().split()
+        keyword_count = sum(
+            [words.count(keyword) for keyword in relevance_keywords]
+        )
+        rows.append(
+            {
+                "keyword_count": keyword_count,
+                "word_count": len(words),
+            }
+        )
+    docs_df = pd.DataFrame(rows, index=ids)
+    docs_df["relevance_score"] = (
+        docs_df["keyword_count"] / docs_df["word_count"]
+    )
+    return docs_df
+
+
+if args.relevance_data_path is None and args.docs_rel_by_id_path is None:
+    fns_all = list(
+        glob.glob(
+            os.path.join(args.openwebtext_folder, "urlsf_subset*-*_data")
+        )
+    )
+
+    rel_dfs = []
+    for fn in tqdm(fns_all):
+        rel_df = get_relevance_of_docs_in_file(fn, RELEVANCE_KEYWORDS)
+        rel_dfs.append(rel_df)
+    all_rel_df = pd.concat(rel_dfs)
+
+    # Save the relevance DataFrame to a timestamped pickle file in the
+    # results folder
+    all_rel_df.to_pickle(
+        os.path.join(
+            args.results_parent_folder,
+            f"relevance_data_{datetime.datetime.now().strftime('%Y%m%dT%H%M%S')}.pkl",
+        )
+    )
+
+elif args.relevance_data_path is not None:
+    # Load previous results from a pickle file as provided in the
+    # argument
+    all_rel_df = pd.read_pickle(args.relevance_data_path)
 
 
 # %%
-# # Scan the dataset to assign a relevance score to each document
-# # (density of relevant keywords)
-# # NOTE: this will take ~25m to run
-# RELEVANCE_KEYWORDS = [
-#     "wedding",
-#     "weddings",
-#     "wed",
-#     "marry",
-#     "married",
-#     "marriage",
-#     "bride",
-#     "groom",
-#     "honeymoon",
-# ]
+# Pick relevant and non-relevent document ids, then make another pass
+# over the dataset to extract the documents with those ids, which are
+# finally saved as a new smaller dataset.
+# NOTE: this will take ~5m to run
 
+if args.docs_rel_by_id_path is None:
+    # Remove duplicate IDs
+    all_rel_df = all_rel_df[~all_rel_df.index.duplicated(keep="first")]
 
-# def load_docs_and_ids(path: str):
-#     """Load documents and associated ids from a file in the OpenWebText
-#     dataset."""
-#     ID_FIELD_LEN = 44  # Used to filter out metadata after splitting on nulls
-#     # Open file and extract documents
-#     with open(path, "r", encoding="utf-8") as file:
-#         fields = re.split("\x00+", file.read())
-#         id = None
-#         ids = []
-#         docs = []
-#         for field in fields:
-#             if field.endswith(".txt") and len(field) == ID_FIELD_LEN:
-#                 id = field
-#             elif id is not None and len(field) > ID_FIELD_LEN:
-#                 ids.append(id)
-#                 docs.append(field)
-#     return ids, docs
+    relevant_num = int(
+        all_rel_df.shape[0] * args.subset_frac * args.relevant_frac
+    )
+    non_relevant_num = int(
+        all_rel_df.shape[0] * args.subset_frac * (1 - args.relevant_frac)
+    )
 
+    # Extract the IDs of the fraction of documents with the highest
+    # relevance scores
+    relevant_ids = (
+        all_rel_df.sort_values(by="relevance_score", ascending=False)
+        .head(relevant_num)
+        .index.tolist()
+    )
 
-# def get_relevance_of_docs_in_file(
-#     path: str, relevance_keywords: Optional[List[str]]
-# ):
-#     """Load documents from a file in the OpenWebText dataset, optionally
-#     calculating the relevance score for each document."""
-#     ids, docs = load_docs_and_ids(path)
-#     # Calculate a relevance score (density of relevant keywords) for
-#     # each doc: the number of relevance keywords in the doc divided by the
-#     # number of words in the doc.
-#     rows = []
-#     for doc in docs:
-#         words = doc.lower().split()
-#         keyword_count = sum(
-#             [words.count(keyword) for keyword in relevance_keywords]
-#         )
-#         rows.append(
-#             {
-#                 "keyword_count": keyword_count,
-#                 "word_count": len(words),
-#             }
-#         )
-#     docs_df = pd.DataFrame(rows, index=ids)
-#     docs_df["relevance_score"] = (
-#         docs_df["keyword_count"] / docs_df["word_count"]
-#     )
-#     return docs_df
+    # Extract a random sample of IDs from the fraction of documents with
+    # relevance score of exactly 0
+    non_relevant_ids = (
+        all_rel_df[all_rel_df["keyword_count"] == 0]
+        .sample(n=non_relevant_num, random_state=args.seed)
+        .index.tolist()
+    )
 
+    # Combine the relevant and non-relevant IDs into a series with index of
+    # IDs and relevance score as values
+    rel_by_id = pd.Series(
+        index=relevant_ids + non_relevant_ids,
+        data=all_rel_df.loc[
+            relevant_ids + non_relevant_ids, "relevance_score"
+        ],
+        name="relevance_score",
+    )
 
-# fns_all = list(
-#     glob.glob(os.path.join(OPENWEBTEXT_FOLDER, "urlsf_subset*-*_data"))
-# )
+    fns_all = list(
+        glob.glob(
+            os.path.join(args.openwebtext_folder, "urlsf_subset*-*_data")
+        )
+    )
 
-# rel_dfs = []
-# for fn in tqdm(fns_all):
-#     rel_df = get_relevance_of_docs_in_file(fn, RELEVANCE_KEYWORDS)
-#     rel_dfs.append(rel_df)
-# all_rel_df = pd.concat(rel_dfs)
+    def get_matching_docs_in_file(path: str, ids_to_keep: pd.Index):
+        """Read ids and docs from an OpenWebText file, returning only the
+        documents whose IDs are in the provided list of ids."""
+        ids, docs = load_docs_and_ids(path)
+        rows = []
+        for id, doc in zip(ids, docs):
+            if id in ids_to_keep:
+                rows.append({"id": id, "doc": doc})
+        return pd.DataFrame(rows).set_index("id") if len(rows) > 0 else None
 
-# # Save the relevance DataFrame to a timestamped pickle file in the
-# # results folder
-# all_rel_df.to_pickle(
-#     os.path.join(
-#         RESULTS_PARENT_FOLDER,
-#         f"relevance_data_{datetime.datetime.now().strftime('%Y%m%dT%H%M%S')}.pkl",
-#     )
-# )
+    # Iterate over the files in the dataset, extracting the docs with IDs
+    # we're interested in and concatenating the results into a single Series
+    # with IDs as index
+    docs_by_id_list = []
+    for fn in tqdm(fns_all):
+        docs_this = get_matching_docs_in_file(fn, rel_by_id.index)
+        if docs_this is not None:
+            docs_by_id_list.append(docs_this)
+    docs_by_id = pd.concat(docs_by_id_list)
 
-# # %%
-# # Pick relevant and non-relevent document ids, then make another pass
-# # over the dataset to extract the documents with those ids, which are
-# # finally saved as a new smaller dataset.
-# # NOTE: this will take ~5m to run
-# SEED = 0
-# TOTAL_FRAC = 0.05
-# RELEVANT_FRAC = TOTAL_FRAC / 2
-# NON_RELEVANT_FRAC = RELEVANT_FRAC
+    # Join in the is_relevant column from the rel_by_id Series
+    docs_rel_by_id = docs_by_id.join(rel_by_id)
 
-# # Load the relevance DataFrame from the pickle file
-# # TODO: load a previously saved file if needed
+    # Save the resulting docs Series to a pickle file in the results folder
+    docs_rel_by_id.to_pickle(
+        os.path.join(
+            args.results_parent_folder,
+            f"docs_rel_by_id_{datetime.datetime.now().strftime('%Y%m%dT%H%M%S')}.pkl",
+        )
+    )
 
-# # Remove duplicate IDs
-# all_rel_df = all_rel_df[~all_rel_df.index.duplicated(keep="first")]
+else:
+    # Load previous results from a pickle file as provided in the
+    # argument
+    docs_rel_by_id = pd.read_pickle(args.docs_rel_by_id_path)
 
-# relevant_num = int(all_rel_df.shape[0] * RELEVANT_FRAC)
-# non_relevant_num = int(all_rel_df.shape[0] * NON_RELEVANT_FRAC)
-
-# # Extract the IDs of the fraction of documents with the highest
-# # relevance scores
-# relevant_ids = (
-#     all_rel_df.sort_values(by="relevance_score", ascending=False)
-#     .head(relevant_num)
-#     .index.tolist()
-# )
-
-# # Extract a random sample of IDs from the fraction of documents with
-# # relevance score of exactly 0
-# non_relevant_ids = (
-#     all_rel_df[all_rel_df["keyword_count"] == 0]
-#     .sample(n=non_relevant_num, random_state=SEED)
-#     .index.tolist()
-# )
-
-# # Combine the relevant and non-relevant IDs into a series with index of
-# # IDs and relevance score as values
-# rel_by_id = pd.Series(
-#     index=relevant_ids + non_relevant_ids,
-#     data=all_rel_df.loc[relevant_ids + non_relevant_ids, "relevance_score"],
-#     name="relevance_score",
-# )
-
-
-# fns_all = list(
-#     glob.glob(os.path.join(OPENWEBTEXT_FOLDER, "urlsf_subset*-*_data"))
-# )
-
-
-# def get_matching_docs_in_file(path: str, ids_to_keep: pd.Index):
-#     """Read ids and docs from an OpenWebText file, returning only the
-#     documents whose IDs are in the provided list of ids."""
-#     ids, docs = load_docs_and_ids(path)
-#     rows = []
-#     for id, doc in zip(ids, docs):
-#         if id in ids_to_keep:
-#             rows.append({"id": id, "doc": doc})
-#     return pd.DataFrame(rows).set_index("id") if len(rows) > 0 else None
-
-
-# # Iterate over the files in the dataset, extracting the docs with IDs
-# # we're interested in and concatenating the results into a single Series
-# # with IDs as index
-# docs_by_id_list = []
-# for fn in tqdm(fns_all):
-#     docs_this = get_matching_docs_in_file(fn, rel_by_id.index)
-#     if docs_this is not None:
-#         docs_by_id_list.append(docs_this)
-# docs_by_id = pd.concat(docs_by_id_list)
-
-# # Join in the is_relevant column from the rel_by_id Series
-# docs_rel_by_id = docs_by_id.join(rel_by_id)
-
-# # Save the resulting docs Series to a pickle file in the results folder
-# docs_rel_by_id.to_pickle(
-#     os.path.join(
-#         RESULTS_PARENT_FOLDER,
-#         f"docs_rel_by_id_{datetime.datetime.now().strftime('%Y%m%dT%H%M%S')}.pkl",
-#     )
-# )
 
 # %%
 # Load a pre-created data subset with document text and relevance scores
 # from a pickle file in the results folder
-docs_rel_by_id = pd.read_pickle(
-    "openwebtext_results/docs_rel_by_id_20230724T142447.pkl"
-)
+# docs_rel_by_id = pd.read_pickle(
+#     "openwebtext_results/docs_rel_by_id_20230724T142447.pkl"
+# )
 
 
 # %%
@@ -254,17 +349,16 @@ mask_len = max([act_add.tokens.size(0) for act_add in activation_additions])
 
 # Create a timestemped results folder in the parent results folder
 RESULTS_FOLDER = os.path.join(
-    RESULTS_PARENT_FOLDER,
+    args.results_parent_folder,
     f"wedding_logprobs_{datetime.datetime.now().strftime('%Y%m%dT%H%M%S')}",
 )
 os.makedirs(RESULTS_FOLDER)
 
 # Iterate over the documents in the dataset, storing logprob data in a
 # dataframe indexed by document ID
-CHUNK_LEN = 100
 docs_rel_by_id_chunks = [
-    docs_rel_by_id.iloc[i : i + CHUNK_LEN]
-    for i in range(0, len(docs_rel_by_id), CHUNK_LEN)
+    docs_rel_by_id.iloc[i : i + args.chunk_len]
+    for i in range(0, len(docs_rel_by_id), args.chunk_len)
 ]
 for chunk_idx, docs_rel_by_id_chunk in enumerate(
     tqdm(list(docs_rel_by_id_chunks))
@@ -319,12 +413,6 @@ for chunk_idx, docs_rel_by_id_chunk in enumerate(
     logprobs_df.to_pickle(
         os.path.join(RESULTS_FOLDER, f"logprobs_df_{chunk_idx}.pkl")
     )
-    # logprobs_df.to_pickle(
-    #     os.path.join(
-    #         RESULTS_PARENT_FOLDER,
-    #         f"logprobs_df_{datetime.datetime.now().strftime('%Y%m%dT%H%M%S')}.pkl",
-    #     )
-    # )
 
 
 # %%
