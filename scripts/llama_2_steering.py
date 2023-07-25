fe5c44a2cb3fa1561700ebd9585e63b895fd293d
DavidUdell <udelldavidb@gmail.com>
2023-07-25 00:21:46 +0000

    Llama 2 70B models require at least transformers 4.31.0.

--- scripts/llama_2_steering.py
+++ scripts/llama_2_steering.py
@@ -1,6 +1,6 @@
 # %%
 """
-Generate HuggingFace Llama-2 prompt completions (up to both the 13B models).
+Generate HuggingFace Llama-2 prompt completions (up to both the 70B models).
 
 Requires a Meta/HuggingFace access token. This will eventually build to steering
 Llama-2, but steering isn't yet implemented.
@@ -10,13 +10,22 @@ from typing import Tuple, Callable, Optional
 import numpy as np
 import torch as t
 from torch import nn
-from transformers import AutoModelForCausalLM, AutoTokenizer, GenerationConfig
-import accelerate
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    BatchEncoding,
+    GenerationConfig,
+    PreTrainedModel,
+    PreTrainedTokenizer,
+)
+from accelerate import Accelerator
 
-# %% # TODO The Llama-2 70B models generate type errors, but the other Llama-2
-# models work fine.
-ACCESS_TOKEN = ""  # Don't accidently commit your access token!
-MODEL_DIR: str = "meta-llama/Llama-2-13b-hf"
+# %%
+# # Note: the Llama-2 70B models require at least `transformers 4.31.0`. I'm
+# not going to put this in requirements.txt yet, in case that breaks other
+# functionality.
+ACCESS_TOKEN: str = ""  # Don't commit HF tokens!
+MODEL_DIR: str = "meta-llama/Llama-2-70b-chat-hf"
 NUM_RETURN_SEQUENCES: int = 1
 MAX_NEW_TOKENS: int = 50
 SEED: int = 0
@@ -43,14 +52,14 @@ t.manual_seed(SEED)
 np.random.seed(SEED)
 
 t.set_grad_enabled(False)
-# A wrapper from accelerate does model parallelization throughout.
-accelerator = accelerate.Accelerator()
-model = AutoModelForCausalLM.from_pretrained(
+# A wrapper from accelerate does the model parallelization throughout.
+accelerator: Accelerator = Accelerator()
+model: PreTrainedModel = AutoModelForCausalLM.from_pretrained(
     MODEL_DIR,
     device_map="auto",
     use_auth_token=ACCESS_TOKEN,
 )
-tokenizer = AutoTokenizer.from_pretrained(
+tokenizer: PreTrainedTokenizer = AutoTokenizer.from_pretrained(
     MODEL_DIR,
     use_auth_token=ACCESS_TOKEN,
 )
@@ -60,14 +69,14 @@ model.tie_weights()
 
 
 # %%
-def tokenize(text: str) -> dict[str, t.Tensor]:
+def tokenize(text: str) -> BatchEncoding:
     """Tokenize prompts onto the appropriate devices."""
     tokens = tokenizer(text, return_tensors="pt")
     return accelerator.prepare(tokens)
 
 
 # %%
-base_tokens = model.generate(
+base_tokens: t.Tensor = model.generate(
     tokenize(PROMPT).input_ids,
     generation_config=GenerationConfig(
         **sampling_kwargs,
@@ -78,8 +87,6 @@ base_tokens = model.generate(
     ),
 )
 print(base_tokens)
-base_strings = [tokenizer.decode(x) for x in base_tokens]
-base_string = ("\n" + "." * 80 + "\n").join(
-    base_strings
-)  # pylint: disable=invalid-name
+base_strings: list[str] = [tokenizer.decode(x) for x in base_tokens]
+base_string: str = ("\n" + "." * 80 + "\n").join(base_strings)
 print(base_string)
