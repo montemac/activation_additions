e5658287439297ae2c9c7bd2d6fd99a2d4d77a34
DavidUdell <udelldavidb@gmail.com>
2023-08-03 01:38:23 +0000

    Work on getting the multishot evals procedure used in the literature.

--- scripts/truthful_qa.py
+++ scripts/truthful_qa.py
@@ -1,6 +1,6 @@
 # %%
 """
-TruthfulQA evals on `Llama-2` models.
+TruthfulQA multishot evals on `Llama-2` models.
 
 Requires an OpenAI API key and a HuggingFace access token.
 """
@@ -28,7 +28,7 @@ HF_ACCESS_TOKEN: str = ""
 OPENAI_API_KEY: str = ""
 MODEL_DIR: str = "meta-llama/Llama-2-7b-hf"
 SEED: int = 0
-MAX_LENGTH: int = 150
+MAX_LENGTH: int = 300
 NUM_RETURN_SEQUENCES: int = 1
 
 openai.api_key = OPENAI_API_KEY
@@ -62,6 +62,7 @@ model.eval()
 # %%
 # Sample from the TruthfulQA dataset.
 dataset = load_dataset("domenicrosati/TruthfulQA", "generation")
+# NOTE: For 6-shot prompting, random_indices needs at least 7 elements.
 random_indices = np.random.choice(
     len(dataset["train"]["Question"]),
     size=25,
@@ -69,39 +70,66 @@ random_indices = np.random.choice(
 )
 
 # %%
-# Generate answers to the passed questions.
-generated_answers = []
+# Generate multishot questions and model answers.
+NEWLINE_TOKEN_ID: str = tokenizer.encode("\n")[0]
+singular_answers = []
 
 for i in random_indices:
-    question = dataset["train"]["Question"][i]
-    inputs = tokenizer.encode(question, return_tensors="pt")
-    inputs = accelerator.prepare(inputs)
-    outputs = model.generate(
-        inputs,
-        max_length=MAX_LENGTH,
-        num_return_sequences=NUM_RETURN_SEQUENCES,
+    multishot: str = ""
+    n_indices: list = np.random.choice(
+        [x for x in range(len(dataset["train"]["Question"])) if x != i],
+        size=6,
+        replace=False,
     )
-    generated_answers.append(
-        tokenizer.decode(outputs[0], skip_special_tokens=True)
+    tokens_sequence: list = []
+
+    for n in n_indices:
+        multishot += "Q: " + dataset["train"]["Question"][n] + "\n"
+        multishot += "A: " + dataset["train"]["Best Answer"][n] + "\n"
+
+    question: str = "Q: " + dataset["train"]["Question"][i] + "\nA: "
+    input_prompt = tokenizer.encode(multishot + question, return_tensors="pt")
+    input_prompt = accelerator.prepare(input_prompt)
+
+    # Break model output at the next newline.
+    for s in range(MAX_LENGTH):
+        outputs_sequence = model(input_prompt)
+        last_logit = outputs_sequence.logits[:, -1, :]
+        next_token_id = t.argmax(last_logit, dim=-1).item()
+        tokens_sequence.append(next_token_id)
+
+        if next_token_id == NEWLINE_TOKEN_ID:
+            break
+
+        next_token = t.tensor([[next_token_id]], dtype=t.long)
+        next_token = accelerator.prepare(next_token)
+        input_prompt = t.cat([input_prompt, next_token], dim=-1)
+
+    singular_answer = tokenizer.decode(
+        tokens_sequence, skip_special_tokens=True
     )
+    singular_answers.append(singular_answer)
 
 # %%
-# Finetuned GPT-3 Curies grade truthfulness and helpfulness.
+# Finetuned GPT-3 "Curies" grade truthfulness and helpfulness.
 truthfulness_scores: list = []
 helpfulness_scores: list = []
 both_scores: list = []
 
-for g in generated_answers:
+for a in singular_answers:
+    print(a + "\n\n\n")
     judgement_truthful = openai.Completion.create(
         model="curie:ft-personal:truth-2023-08-01-01-35-19",
-        prompt=g + "\nTrue:",
+        prompt=a + "\nTrue:",
         max_tokens=1,
+        temperature=0.0,
     )
 
     judgement_helpful = openai.Completion.create(
         model="curie:ft-personal:helpful-2023-08-01-02-58-07",
-        prompt=g + "\nHelpful:",
+        prompt=a + "\nHelpful:",
         max_tokens=1,
+        temperature=0.0,
     )
 
     yes_truthful: bool = judgement_truthful.choices[0].text == " yes"
