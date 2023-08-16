c94ffebbf94feee02b4e32d6dccca86dede538a9
DavidUdell <udelldavidb@gmail.com>
2023-08-16 01:51:07 +0000

    It is now a variational autoencoder, and will train.

--- experiments/autoencoder_training.py
+++ experiments/autoencoder_training.py
@@ -1,11 +1,16 @@
 # %%
-"""Dictionary learning on an activations dataset using a variational autoencoder."""
+"""Dict learning on an activations dataset, with a variational autoencoder."""
 
 
 import torch as t
 import pytorch_lightning as pl
 from torch.utils.data import DataLoader, Dataset
 
+
+# %%
+# Training hyperparameters. We want to weight L1 extremely heavily.
+LAMBDA_L1: float = 1.0
+LAMBDA_KL: float = 1e-9
 
 # %%
 # Use available tensor cores.
@@ -46,58 +51,81 @@ dataloader: DataLoader = DataLoader(
 class Autoencoder(pl.LightningModule):
     """A variational autoencoder architecture."""
 
+
     def __init__(self):
         super().__init__()
-        # TODO: Add in the _variational_ component. The first linear layer
-        # learns a matrix map to a higher-dimensional space. That projection
-        # matrix is what I'm intersted in here.
+        # The first linear layer learns a matrix map to a higher-dimensional
+        # space. That projection matrix is what I'm intersted in here.
         self.encoder = t.nn.Sequential(
             t.nn.Linear(4096, 8192),
             t.nn.ReLU(),
+            t.nn.Linear(8192, 8192 * 2)
         )
 
-        # The second linear map returns us to the original activation space, so
-        # we can evaluate our reconstruction loss.
+        # The second linear map just returns us to the original activation
+        # space, so we can evaluate our reconstruction loss.
         self.decoder = t.nn.Sequential(
             t.nn.Linear(8192, 4096),
         )
 
+
     def forward(self, state):  # pylint: disable=arguments-differ
-        """The forward pass of the autoencoder. Information is compacted, then reconstructed."""
+        """The forward pass of a variational autoencoder for activations."""
         encoded_state = self.encoder(state)
-        output_state = self.decoder(encoded_state)
-        return encoded_state, output_state
+        mean, logvar = encoded_state.split(8192, dim=-1)
+
+        # Sample from the encoder normal distribution.
+        std = t.exp(0.5 * logvar)  # pylint: disable=no-member
+        epsilon = t.randn_like(std)  # pylint: disable=no-member
+        sampled_state = mean + (epsilon * std)
+
+        # Decode the sampled state.
+        output_state = self.decoder(sampled_state)
+        return mean, logvar, sampled_state, output_state
+
 
     def training_step(self, batch):  # pylint: disable=arguments-differ
         """Train the autoencoder."""
         state = batch
-        encoded_state, output_state = self.forward(state)
+        mean, logvar, sampled_state, output_state = self.forward(state)
 
-        # I want to learn a disentangled, sparse representation of the original
-        # learned features present in the training activations. L1
-        # regularization in the higher-dimensional space does this.
-        l1_loss = t.nn.functional.l1_loss(encoded_state, t.zeros_like(encoded_state))  # pylint: disable=no-member
+        # For the statistical component of the forward pass.
+        kl_loss = -0.5 * t.sum(1 + logvar - mean.pow(2) - logvar.exp())  # pylint: disable=no-member
+
+        # I want to learn a _sparse representation_ of the original learned
+        # features present in the training activations. L1 regularization in the
+        # higher-dimensional space does this.
+        l1_loss = t.nn.functional.l1_loss(sampled_state, t.zeros_like(sampled_state))  # pylint: disable=no-member
 
         # I also need to inventivize learning features that match the originals.
         # I project back to the original space, then evaluate MSE for this.
         mse_loss = t.nn.functional.mse_loss(output_state, state)
 
-        # The total loss function just combines the two above terms.
-        lambda_l1: float = 1e-1
-        loss = mse_loss + lambda_l1 * l1_loss
-        
-        # TODO: Log sparsity data along with training loss.
-        self.log("training_loss", loss)
+        # The overall loss function just combines the three above terms.
+        loss = mse_loss + (LAMBDA_L1 * l1_loss) + (LAMBDA_KL * kl_loss)
+
+        self.log("loss", loss)
+
+        self.log("L1 sub-loss", l1_loss)
+        self.log("KL sub-loss", kl_loss)
+        self.log("MSE sub-loss", mse_loss)
         return loss
+
 
     def configure_optimizers(self):
         """Configure the optimizer (`Adam`)."""
-        return t.optim.Adam(self.parameters(), lr=1e-3)
+        return t.optim.Adam(self.parameters(), lr=1e-4)
 
 
 # %%
 # Train the autoencoder.
 model: Autoencoder = Autoencoder()
-
 trainer: pl.Trainer = pl.Trainer(accelerator="auto", max_epochs=150, log_every_n_steps=1)
+
 trainer.fit(model, dataloader)
+
+# %%
+# Save the learned projection matrix.
+t.save(
+    model.encoder[0].weight.data, "/root/algebraic_value_editing/experiments/projection_matrix.pt"
+)
