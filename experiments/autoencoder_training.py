ae1006294f2179f08b94b982dac5156a814ac829
DavidUdell <udelldavidb@gmail.com>
2023-08-16 21:39:57 +0000

    Progress writing the meanings testbed script.

--- experiments/autoencoder_training.py
+++ experiments/autoencoder_training.py
@@ -38,11 +38,14 @@ class ActivationsDataset(Dataset):
 # %%
 # Put the dataset into a dataloader.
 dataset: ActivationsDataset = ActivationsDataset(
-    "/root/algebraic_value_editing/experiments/activations_dataset.pt"
-    )
+    "/mnt/ssd-2/mesaoptimizer/david/activation_additions/Llama-2_7B_activations.pt"
+)
 
 dataloader: DataLoader = DataLoader(
-    dataset, batch_size=32, shuffle=True, num_workers=16,
+    dataset,
+    batch_size=32,
+    shuffle=True,
+    num_workers=16,
 )
 
 
@@ -51,7 +54,6 @@ dataloader: DataLoader = DataLoader(
 class Autoencoder(pl.LightningModule):
     """A variational autoencoder architecture."""
 
-
     def __init__(self):
         super().__init__()
         # The first linear layer learns a matrix map to a higher-dimensional
@@ -59,16 +61,13 @@ class Autoencoder(pl.LightningModule):
         # second linear map tees up the variational components of the
         # architecture.
         self.encoder = t.nn.Sequential(
-            t.nn.Linear(4096, 8192),
-            t.nn.ReLU(),
-            t.nn.Linear(8192, 8192 * 2)
+            t.nn.Linear(4096, 8192), t.nn.ReLU(), t.nn.Linear(8192, 8192 * 2)
         )
         # The decoder's linear map just returns us to the original activation
         # space, so we can evaluate our reconstruction loss.
         self.decoder = t.nn.Sequential(
             t.nn.Linear(8192, 4096),
         )
-
 
     def forward(self, state):  # pylint: disable=arguments-differ
         """The forward pass of a variational autoencoder for activations."""
@@ -84,19 +83,22 @@ class Autoencoder(pl.LightningModule):
         output_state = self.decoder(sampled_state)
         return mean, logvar, sampled_state, output_state
 
-
     def training_step(self, batch):  # pylint: disable=arguments-differ
         """Train the autoencoder."""
         state = batch
         mean, logvar, sampled_state, output_state = self.forward(state)
 
         # For the statistical component of the forward pass.
-        kl_loss = -0.5 * t.sum(1 + logvar - mean.pow(2) - logvar.exp())  # pylint: disable=no-member
+        kl_loss = -0.5 * t.sum(
+            1 + logvar - mean.pow(2) - logvar.exp()
+        )  # pylint: disable=no-member
 
         # I want to learn a _sparse representation_ of the original learned
         # features present in the training activations. L1 regularization in the
         # higher-dimensional space does this.
-        l1_loss = t.nn.functional.l1_loss(sampled_state, t.zeros_like(sampled_state))  # pylint: disable=no-member
+        l1_loss = t.nn.functional.l1_loss(
+            sampled_state, t.zeros_like(sampled_state)
+        )  # pylint: disable=no-member
 
         # I also need to inventivize learning features that match the originals.
         # I project back to the original space, then evaluate MSE for this.
@@ -112,7 +114,6 @@ class Autoencoder(pl.LightningModule):
 
         return loss
 
-
     def configure_optimizers(self):
         """Configure the optimizer (`Adam`)."""
         return t.optim.Adam(self.parameters(), lr=1e-5)
@@ -121,12 +122,15 @@ class Autoencoder(pl.LightningModule):
 # %%
 # Train the autoencoder.
 model: Autoencoder = Autoencoder()
-trainer: pl.Trainer = pl.Trainer(accelerator="auto", max_epochs=150, log_every_n_steps=25)
+trainer: pl.Trainer = pl.Trainer(
+    accelerator="auto", max_epochs=150, log_every_n_steps=25
+)
 
 trainer.fit(model, dataloader)
 
 # %%
 # Save the learned projection matrix.
 t.save(
-    model.encoder[0].weight.data, "/root/algebraic_value_editing/experiments/maps_acts_to_features.pt"
+    model.encoder[0].weight.data,
+    "/mnt/ssd-2/mesaoptimizer/david/activation_additions/Llama-2_7B_decoder.pt",
 )
