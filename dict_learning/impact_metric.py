e7e74f1f413f3de51d9631e21ce33301e7d17530
DavidUdell <udelldavidb@gmail.com>
2023-08-21 21:45:18 +0000

    First pass batching code. Still has some dim length inconsistencies to iron out.

--- dict_learning/impact_metric.py
+++ dict_learning/impact_metric.py
@@ -37,6 +37,7 @@ MODEL_DIR: str = "meta-llama/Llama-2-7b-hf"
 DECODER_PATH: str = "acts_data/learned_decoder.pt"
 IMPACT_SAVE_PATH: str = "acts_data/impacts.csv"
 SEED: int = 0
+BATCH_SIZE: int = 10
 INJECTION_LAYER: int = 16  # Layer to _add_ feature directions to.
 COEFF: float = 2.0  # Coefficient for the feature addition.
 MAX_NEW_TOKENS: int = 1
@@ -153,8 +154,81 @@ def unhot(labels: list) -> int:
 
 
 # %%
+# Build multishot prompts.
+def build_multishot_prompt(q_num: int, num_shot: int) -> str:
+    """
+    Build up the multishot prompt for a question index.
+
+    Implicitly relies on `dataset` and `unhot`.
+    """
+    multishot: str = ""
+    # Sample multishot questions that _aren't_ the current question.
+    multishot_indices: ndarray = np.random.choice(
+        [
+            x
+            for x in range(len(dataset["validation"]["question"]))
+            if x != q_num
+        ],
+        size=num_shot,
+        replace=False,
+    )
+
+    # Build the multishot prompt.
+    for mult_num in multishot_indices:
+        multishot += "Q: " + dataset["validation"]["question"][mult_num] + "\n"
+
+        for choice_num in range(
+            len(dataset["validation"]["mc1_targets"][mult_num]["choices"])
+        ):
+            # choice_num is 0-indexed, but I want to display _1-indexed_
+            # options.
+            multishot += (
+                "("
+                + str(choice_num + 1)
+                + ") "
+                + dataset["validation"]["mc1_targets"][mult_num]["choices"][
+                    choice_num
+                ]
+                + "\n"
+            )
+
+        labels_one_hot: list = dataset["validation"]["mc1_targets"][mult_num][
+            "labels"
+        ]
+        # Get a label int from the `labels` list.
+        correct_answer: int = unhot(labels_one_hot)
+        # Add on the correct answer under each multishot question.
+        multishot += "A: (" + str(correct_answer) + ")\n"
+
+    # Add on the current question.
+    question: str = "Q: " + dataset["validation"]["question"][q_num] + "\n"
+    for option_num in range(
+        len(dataset["validation"]["mc1_targets"][q_num]["choices"])
+    ):
+        # option_num is similarly 0-indexed, but I want 1-indexed options
+        # here too.
+        question += (
+            "("
+            + str(option_num + 1)
+            + ") "
+            + dataset["validation"]["mc1_targets"][q_num]["choices"][
+                option_num
+            ]
+            + "\n"
+        )
+    # I only want the model to actually answer the question, with a single
+    # token, so I tee it up here with the opening parentheses to a
+    # multiple-choice answer integer.
+    question += "A: ("
+
+    return multishot + question
+
+
+# %%
 # The basic evals loop.
-def mc_evals(samples_nums: list, num_shot: int = NUM_SHOT) -> dict[int, float]:
+def mc_evals(
+    samples_nums: list, batch_size: int = BATCH_SIZE, num_shot: int = NUM_SHOT
+) -> dict[int, float]:
     """
     Run the eval loop, collecting the logit on ground truth for each question.
 
@@ -164,90 +238,45 @@ def mc_evals(samples_nums: list, num_shot: int = NUM_SHOT) -> dict[int, float]:
     """
     ground_truth_logits: dict = {}
 
-    for question_num in samples_nums:
-        multishot: str = ""
-        # Sample multishot questions that _aren't_ the current question.
-        multishot_indices: ndarray = np.random.choice(
-            [
-                x
-                for x in range(len(dataset["validation"]["question"]))
-                if x != question_num
-            ],
-            size=num_shot,
-            replace=False,
+    # Get slice indices.
+    for batch_first in range(0, len(samples_nums), batch_size):
+        batch_indices: list = samples_nums[
+            batch_first : (batch_first + batch_size)
+        ]
+        batch_inputs: list = []
+
+        for question_num in batch_indices:
+            prompt: str = build_multishot_prompt(question_num, num_shot)
+
+            # Tokenize, prepare the model input.
+            input_ids: t.Tensor = tokenizer.encode(prompt, return_tensors="pt")
+            input_ids = accelerator.prepare(input_ids)
+            batch_inputs.append(input_ids)
+
+        # Pad, tensorize, and prepare the batch.
+        batch_inputs = t.nn.utils.rnn.pad_sequence(
+            batch_inputs, batch_first=True
         )
+        batch_inputs = accelerator.prepare(batch_inputs)
 
-        # Build the multishot prompt.
-        for mult_num in multishot_indices:
-            multishot += (
-                "Q: " + dataset["validation"]["question"][mult_num] + "\n"
-            )
-
-            for choice_num in range(
-                len(dataset["validation"]["mc1_targets"][mult_num]["choices"])
-            ):
-                # choice_num is 0-indexed, but I want to display _1-indexed_
-                # options.
-                multishot += (
-                    "("
-                    + str(choice_num + 1)
-                    + ") "
-                    + dataset["validation"]["mc1_targets"][mult_num][
-                        "choices"
-                    ][choice_num]
-                    + "\n"
-                )
-
-            labels_one_hot: list = dataset["validation"]["mc1_targets"][
-                mult_num
-            ]["labels"]
-            # Get a label int from the `labels` list.
-            correct_answer: int = unhot(labels_one_hot)
-            # Add on the correct answer under each multishot question.
-            multishot += "A: (" + str(correct_answer) + ")\n"
-
-        # Add on the current question.
-        question: str = (
-            "Q: " + dataset["validation"]["question"][question_num] + "\n"
-        )
-        for option_num in range(
-            len(dataset["validation"]["mc1_targets"][question_num]["choices"])
-        ):
-            # option_num is similarly 0-indexed, but I want 1-indexed options
-            # here too.
-            question += (
-                "("
-                + str(option_num + 1)
-                + ") "
-                + dataset["validation"]["mc1_targets"][question_num][
-                    "choices"
-                ][option_num]
-                + "\n"
-            )
-        # I only want the model to actually answer the question, with a single
-        # token, so I tee it up here with the opening parentheses to a
-        # multiple-choice answer integer.
-        question += "A: ("
-
-        # Tokenize, prepare the model input.
-        input_ids: t.Tensor = tokenizer.encode(
-            multishot + question, return_tensors="pt"
-        )
-        input_ids = accelerator.prepare(input_ids)
-
-        # Generate a single-token completion.
-        outputs = model(input_ids)
+        # Generate a single-token completion for each batched input.
+        outputs = model(batch_inputs)
 
         # Get the ground truth logit. The ground truth is ultimately stored as a
         # string literal, so I have to work a bit to get its logit from the
         # model's final logits.
-        ground_truth_one_hot: list = dataset["validation"]["mc1_targets"][
-            question_num
-        ]["labels"]
-        ground_truth_ans: int = unhot(ground_truth_one_hot)
-        ground_truth_id: list[int] = tokenizer.encode(str(ground_truth_ans))
-        ground_truth_logit: float = outputs.logits[0, -1, ground_truth_id[0]]
-        ground_truth_logits[question_num] = ground_truth_logit.item()
+        for question_num in batch_indices:
+            ground_truth_one_hot: list = dataset["validation"]["mc1_targets"][
+                question_num
+            ]["labels"]
+            ground_truth_ans: int = unhot(ground_truth_one_hot)
+            ground_truth_id: list[int] = tokenizer.encode(
+                str(ground_truth_ans)
+            )
+            ground_truth_logit: float = outputs.logits[
+                0, -1, ground_truth_id[0]
+            ]
+            ground_truth_logits[question_num] = ground_truth_logit.item()
 
     return ground_truth_logits
 
