fb8621b96ba946d951ad8b477262b73ada92876d
DavidUdell <udelldavidb@gmail.com>
2023-08-24 01:35:07 +0000

    Cleaning up the visualization object manipluations to debug.

--- sparse_coder/meanings_visualization.py
+++ sparse_coder/meanings_visualization.py
@@ -58,12 +58,11 @@ class Decoder:
         return self.decoder(inputs)
 
 
-projector = Decoder()
+model = Decoder()
 
 # %%
-# Load and prep the activations and tokens.
+# Load and prep the tokens.
 prompts_ids: np.ndarray = np.load(PROMPT_IDS_PATH, allow_pickle=True)
-acts_dataset: t.Tensor = t.load(ACTS_DATA_PATH)
 
 # Convert the prompt_ids into lists of strings.
 prompts_literals: list = []
@@ -72,20 +71,67 @@ for p in prompts_ids:
     prompt_literal: list = tokenizer.convert_ids_to_tokens(p.squeeze())
     prompts_literals.append(prompt_literal)
 
-# We use the prompt_ids to remove the activation data padding.
-unpadded_activations: list = []
-projected_activations: list = []
-for i, activation in enumerate(acts_dataset):
-    prompt_length: int = len(prompts_ids[i])
-    unpadded_activation = activation[:prompt_length, :]
-    unpadded_activations.append(unpadded_activation)
+# %%
+# Load and prep the cached activations.
+acts_dataset: t.Tensor = t.load(ACTS_DATA_PATH)
 
-for a in unpadded_activations:
-    projected_activation = projector(a).detach()
-    projected_activation.unsqueeze(0)
-    projected_block = t.cat(a, dim=0)  # pylint: disable=no-member
-    projected_activations.append(projected_block)
 
+def unpad_activations(
+    activations_block: t.Tensor, unpadded_prompts: np.ndarray
+) -> t.Tensor:
+    """
+    Unpads activations to the lengths specified by the original prompts.
+
+    Note that the activation block must come in with dimensions (batch x stream
+    x embedding_dim), and the unpadded prompts as an array of lists of
+    elements.
+    """
+    unpadded_activations: list = []
+
+    # Iterating over the tensor now.
+    for i in range(len(activations_block)):
+        print(unpadded_prompts.shape(0))
+        # Since the unpadded_prompt still represents the original tokenized
+        # length, we slice the activations to its length.
+        unpadded_activations.append(
+            activations_block[i, : len(unpadded_prompts[i]), :]
+        )
+
+    return t.stack(unpadded_activations)  # pylint: disable=no-member
+
+
+def project_activations(
+    activations_block: t.Tensor, projector: Decoder
+) -> t.Tensor:
+    """Projects the activations block over to the sparse latent space."""
+    projected_activations: list = []
+
+    # Iterate over the tensor batch dim.
+    for activations in activations_block:
+        # Detach the gradients from the model pass.
+        projected_activations.append(projector(activations).detach())
+
+    return t.stack(projected_activations)  # pylint: disable=no-member
+
+
+def rearrange_for_vis(activations_block: t.Tensor) -> [t.Tensor]:
+    """`circuitsvis` wants inputs [(stream x layers x embedding_dim)]."""
+
+    # Rearrange the activations from (batch x stream x embedding_dim) to
+    # [(stream x layer x embedding_dim)]. Note that batch comes apart into a
+    # list of tensors, and layer is always of size 1.
+    rearranged_activations: list = []
+
+    for activations in activations_block:
+        rearranged_activations.append(
+            t.unsqueeze(activations, 1)  # pylint: disable=no-member
+        )
+
+    return rearranged_activations
+
+
+unpadded = unpad_activations(acts_dataset, prompts_ids)
+print(unpadded.shape)
 
 # %%
 # Visualize the activations.
