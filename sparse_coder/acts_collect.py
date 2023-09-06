4bdef9227582a8dc7cf1bad10ae168705eaf8c4e
DavidUdell <udelldavidb@gmail.com>
2023-09-06 22:15:57 +0000

    YAML error handling.

--- sparse_coder/acts_collect.py
+++ sparse_coder/acts_collect.py
@@ -32,11 +32,17 @@ assert (
 
 # %%
 # Set up constants.
-with open("act_access.yaml", "r", encoding="utf-8") as f:
-    try:
+try:
+    with open("act_access.yaml", "r", encoding="utf-8") as f:
         access = yaml.safe_load(f)
-    except yaml.YAMLError as e:
-        print(e)
+except FileNotFoundError:
+    print("act_access.yaml not found. Creating it now.")
+    with open("act_access.yaml", "w", encoding="utf-8") as w:
+        w.write('HF_ACCESS_TOKEN: ""\n')
+    access = {}
+except yaml.YAMLError as e:
+    print(e)
+
 with open("act_config.yaml", "r", encoding="utf-8") as f:
     try:
         config = yaml.safe_load(f)
