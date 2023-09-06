4209968991f7a9d0abd2f64203b059e2b42d68e7
DavidUdell <udelldavidb@gmail.com>
2023-09-06 21:45:41 +0000

    Added to setup.py.

--- setup.py
+++ setup.py
@@ -11,6 +11,11 @@ setup(
     version="0.2.0",
     packages=find_packages(),
     install_requires=[
+        "accelerate",
+        "jupyter",
+        "lightning",
+        "scikit-learn",
+        "PyYAML",
         "transformer_lens",
         "torch==1.13.1",
         "numpy>=1.22.1",
