2b173363b781ec2f47071ee15bea62d6a464746c
DavidUdell <udelldavidb@gmail.com>
2023-10-12 23:58:41 +0000

    Started filling out the unit tests of core functions.

--- tests/test_sparse_coding.py
+++ tests/test_sparse_coding.py
@@ -1,33 +1,60 @@
 """Unit tests for the `sparse_coding` submodule."""
 
 
-from unittest.mock import patch
+from collections import defaultdict
 
-import numpy as np
-import pytest
+import accelerate
 import torch as t
 import transformers
-import yaml
 
-from sparse_coding.acts_collect import (
-    shuffle_answers,
-    unhot,
-    pad_activations,
-)
+from sparse_coding.utils.top_k import calculate_effects, project_activations
+from sparse_coding.acts_collect import pad_activations
+from sparse_coding.autoencoder import padding_mask
+from sparse_coding.feature_tokens import unpad_activations, select_top_k_tokens
 
-from sparse_coding.autoencoder import (
-    padding_mask,
-    ActivationsDataset,
-    Autoencoder,
-)
 
-from sparse_coding.feature_tokens import (
-    Encoder,
-    unpad_activations,
-    project_activations,
-    select_top_k_tokens,
-    round_floats,
-    populate_table,
-)
+def test_calulate_effects():
+    """Test the `calculate_effects` function."""
 
-from sparse_coding.utils.top_k import calculate_effects
+    question_token_ids: list[list[int]] = [[0, 0, 0, 0, 0], [1, 1, 1, 1, 1]]
+    feature_activations: list[t.Tensor] = []
+    model = transformers.AutoModelForCausalLM.from_pretrained(
+        "EleutherAI/pythia-70m"
+    )
+    tokenizer = transformers.AutoTokenizer.from_pretrained(
+        "EleutherAI/pythia-70m"
+    )
+    accelerator = accelerate.Accelerator()
+    batch_size = 2
+
+    effects = calculate_effects(
+        question_token_ids,
+        feature_activations,
+        model,
+        tokenizer,
+        accelerator,
+        batch_size,
+    )
+
+    assert isinstance(effects, defaultdict)
+    assert len(effects) ==
+
+
+def test_project_activations():
+    pass
+
+
+def test_pad_activations():
+    pass
+
+
+def test_padding_mask():
+    pass
+
+
+def test_unpad_activations():
+    pass
+
+
+def test_select_top_k_tokens():
+    pass
